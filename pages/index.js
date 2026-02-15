9f64279cdbfb9b8aa4165b0ac2532dc87701f6b7
Yoshi <yoshio.nasu@extool.co.jp>
2026-02-15 09:05:45 +0900

    Add: カスタムファビコンとページメタデータの設定
    - チェックリストアイコンをファビコンに設定
    - SVGファビコンを追加 (public/favicon.svg)
    - ページタイトルとメタデータを追加
    - Next.js Head コンポーネントを使用

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,32 @@
+import Head from 'next/head'
 import TaskManagementBoard from '../components/TaskManagementBoard'
 import DailyTodo from '../components/DailyTodo'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <div className="max-w-7xl mx-auto p-6 md:p-8">
-        <h1 className="text-3xl font-bold text-gray-800 mb-6">
-          TaskManagementBoard
-        </h1>
-        
-        {/* 本日のToDo */}
-        <div className="mb-8">
-          <DailyTodo />
-        </div>
+    <>
+      <Head>
+        <title>TaskManagementBoard - タスク管理</title>
+        <meta name="description" content="プロジェクトとタスクを一元管理するダッシュボード" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      
+      <div className="min-h-screen bg-gray-100">
+        <div className="max-w-7xl mx-auto p-6 md:p-8">
+          <h1 className="text-3xl font-bold text-gray-800 mb-6">
+            TaskManagementBoard
+          </h1>
+          
+          {/* 本日のToDo */}
+          <div className="mb-8">
+            <DailyTodo />
+          </div>
 
-        {/* プロジェクト管理 */}
-        <TaskManagementBoard />
+          {/* プロジェクト管理 */}
+          <TaskManagementBoard />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
